448079d85f8c9eb3353c56bf08bcf260b1fd96ab
TarunJCole <elocnurat@hotmail.com>
2019-07-15 16:02:04 +0100

    event handlers + searchbar controlled component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 // Main App
 import React, { Component } from "react";
+import axios from "axios";
 import "../style/App.css";
 
 import Searchbar from "./Searchbar";
 
 export default class App extends Component {
+    state = {
+        searchTerm: "",
+        imageResults: []
+    };
+
+    onSearchSubmit = event => {
+        event.preventDefault();
+        console.log("submitted");
+    };
+
     render() {
         return (
             <div>
-                <Searchbar />
+                <Searchbar onSearchSubmit={this.onSearchSubmit} />
             </div>
         );
     }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
 export default class Searchbar extends Component {
+    state = {
+        term: ""
+    };
+
+    onInputChange = event => {
+        this.setState({ term: event.target.value });
+        console.log(this.state.term);
+    };
+
     render() {
         return (
-            <div class="searchbar">
+            <div className="searchbar">
                 <i className="fas fa-search" />
-                <input type="text" />
+                <form onSubmit={this.props.onSearchSubmit}>
+                    <input
+                        type="text"
+                        onChange={this.onInputChange}
+                        value={this.state.term}
+                    />
+                </form>
             </div>
         );
     }
