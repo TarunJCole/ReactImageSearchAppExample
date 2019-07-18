6b34fcf9565397a6f662d1af1d75db248d722006
TarunJCole <elocnurat@hotmail.com>
2019-07-18 16:50:01 +0100

    update modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,7 +8,7 @@ export default class Modal extends Component {
                     className="close-button"
                     onClick={() => this.props.changeModalImage(null)}
                 >
-                    <i class="fas fa-times fa-3x" />
+                    <i className="fas fa-times fa-3x" />
                 </button>
 
                 <div id="modal-content">
@@ -16,6 +16,31 @@ export default class Modal extends Component {
                         src={this.props.image.urls.full}
                         alt={this.props.image.alt_description}
                     />
+                    <div>
+                        <div className="user">
+                            <img
+                                src={this.props.image.user.profile_image.medium}
+                                alt="user avatar"
+                            />
+                            <span>{this.props.image.user.name}</span>
+                        </div>
+                        <span>
+                            <i className="far fa-thumbs-up fa-2x" />{" "}
+                            {this.props.image.likes}
+                        </span>
+                        <span>
+                            <i className="fas fa-link fa-2x" />
+                            <a href={this.props.image.links.html}>
+                                View On Unsplash
+                            </a>
+                        </span>
+                        <span>
+                            <i className="fas fa-download fa-2x" />
+                            <a href={this.props.image.links.download}>
+                                Download
+                            </a>
+                        </span>
+                    </div>
                 </div>
             </div>
         );
